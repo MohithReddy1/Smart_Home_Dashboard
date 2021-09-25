51b8757163483c581637de1f821260802010278d
MohithReddy1 <vuyyuru.reddy.19cs@bmu.edu.in>
2021-09-25 09:21:42 +0530

    datetime

--- Smart_Home.js
+++ Smart_Home.js
@@ -1,12 +1,12 @@
 const gaugeElement = document.querySelector(".gauge");
 
 function setGaugeValue(gauge, value) {
-if (value < 0 || value > 1) {
-    return;
-}
+    if (value < 0 || value > 1) {
+        return;
+    }
 
-gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
-gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
+    gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
+    gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
 }
 
 setGaugeValue(gaugeElement, 0.6);
@@ -16,11 +16,11 @@ function light1() {
     let lightimg1 = document.getElementById("lightimg1");
     let lighton1 = document.getElementById("lighton1");
 
-    if(lightswitch1.checked === true){
+    if (lightswitch1.checked === true) {
         lightimg1.style.display = "none";
         lighton1.style.display = "block";
     }
-    else{
+    else {
         lightimg1.style.display = "block";
         lighton1.style.display = "none";
     }
@@ -32,11 +32,11 @@ function light2() {
     let lightimg2 = document.getElementById("lightimg2");
     let lighton2 = document.getElementById("lighton2");
 
-    if(lightswitch2.checked === true){
+    if (lightswitch2.checked === true) {
         lightimg2.style.display = "none";
         lighton2.style.display = "block";
     }
-    else{
+    else {
         lightimg2.style.display = "block";
         lighton2.style.display = "none";
     }
@@ -48,11 +48,11 @@ function light3() {
     let lightimg3 = document.getElementById("lightimg3");
     let lighton3 = document.getElementById("lighton3");
 
-    if(lightswitch3.checked === true){
+    if (lightswitch3.checked === true) {
         lightimg3.style.display = "none";
         lighton3.style.display = "block";
     }
-    else{
+    else {
         lightimg3.style.display = "block";
         lighton3.style.display = "none";
     }
@@ -64,11 +64,11 @@ function bedlamp1() {
     let bedlampimg1 = document.getElementById("bedlampimg1");
     let bedlampon1 = document.getElementById("bedlampon1");
 
-    if(bedlampswitch1.checked === true){
+    if (bedlampswitch1.checked === true) {
         bedlampimg1.style.display = "none";
         bedlampon1.style.display = "block";
     }
-    else{
+    else {
         bedlampimg1.style.display = "block";
         bedlampon1.style.display = "none";
     }
@@ -88,10 +88,10 @@ let fanswitch4 = document.getElementById("fanswitch4");
 let fan4 = document.getElementById("fan4");
 
 function fanrotate1() {
-    if(fanswitch1.checked === true){
+    if (fanswitch1.checked === true) {
         fan1.className += " rotate";
     }
-    else{
+    else {
         fan1.className -= " rotate";
     }
 
@@ -99,20 +99,20 @@ function fanrotate1() {
 
 function fanrotate2() {
 
-    if(fanswitch2.checked === true){
+    if (fanswitch2.checked === true) {
         fan2.className += " rotate";
     }
-    else{
+    else {
         fan2.className -= " rotate";
     }
 
 }
 
 function fanrotate3() {
-    if(fanswitch3.checked === true){
+    if (fanswitch3.checked === true) {
         fan3.className += " rotate";
     }
-    else{
+    else {
         fan3.className -= " rotate";
     }
 
@@ -120,59 +120,38 @@ function fanrotate3() {
 
 function fanrotate4() {
 
-    if(fanswitch4.checked === true){
+    if (fanswitch4.checked === true) {
         fan4.className += " rotate";
     }
-    else{
+    else {
         fan4.className -= " rotate";
     }
 
 }
 
-let range1 = document.getElementById("range1");
-range1.value = 0;
-
-function fanrange1(){
-    if(fanswitch1.checked === true){
-        range1.value = 1;
-    }
-    else if(fanswitch1.checked === false){
-        range1.value = 0;
-    }
+function startTime() {
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    let options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    };
+    let timeString = today.toLocaleString('en-US', options);
+    document.getElementById('date').innerHTML = day + "/" + month + "/" + year;
+    // document.getElementById('time').innerHTML = h + ":" + m;
+    document.getElementById('time').innerHTML = timeString;
+    let t = setTimeout(function () { startTime() }, 500);
 }
 
-let range2 = document.getElementById("range2");
-range2.value = 0;
-
-function fanrange2(){
-    if(fanswitch2.checked === true){
-        range2.value = 1;
-    }
-    else if(fanswitch2.checked === false){
-        range2.value = 0;
-    }
-}
-
-let range3 = document.getElementById("range3");
-range3.value = 0;
-
-function fanrange3(){
-    if(fanswitch3.checked === true){
-        range3.value = 1;
-    }
-    else if(fanswitch3.checked === false){
-        range3.value = 0;
-    }
-}
-
-let range4 = document.getElementById("range4");
-range4.value = 0;
-
-function fanrange4(){
-    if(fanswitch4.checked === true){
-        range4.value = 1;
-    }
-    else if(fanswitch4.checked === false){
-        range4.value = 0;
-    }
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
 }
